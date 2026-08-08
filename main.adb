d463055cd4e52a6d6a458ac17c24d473a1caa28d
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-08 15:42:18 +0200

    Create main.adb

---
+++ main.adb
@@ -0,0 +1,8 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Vector_Clocks; use Vector_Clocks;
+
+procedure Main is
+begin
+   Put_Line ("Vector Clocks Algorithm implementation initialized.");
+   Put_Line ("Please run 'make test' to execute the comprehensive V&V test suite.");
+end Main;
